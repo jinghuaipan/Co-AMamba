9088fc8f33d997d239d65c6a3964a5726a6225b8
jinghuaipan <156070354+jinghuaipan@users.noreply.github.com>
2025-06-07 21:56:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -47,12 +47,6 @@ parser.add_argument('--size',
                     default=256,
                     type=int,
                     help='input size')
-
-# parser.add_argument('--ckpt_dir',
-# default='./ckpt',
-# help='Temporary folder')
-
-
 parser.add_argument('--save_root', default='./data/pred', type=str, help='Output folder')
 parser.add_argument('--tmp', default='./weight/', help='Temporary folder')
 
@@ -60,7 +54,6 @@ args = parser.parse_args()
 
 config = Config()
 
-# Prepare dataset
 root_dir = './trainsets/sod/'
 if 'seg_duts' in args.trainset.split('+'):
     train_img_path = os.path.join(root_dir, 'images/seg_duts')
@@ -90,22 +83,15 @@ for testsets in args.testsets.split('+'):
     )
     test_loaders[testsets] = test_loader
 
-# if config.rand_seed:
-#     set_seed(config.rand_seed)
-
-# make dir for ckpt
-# os.makedirs(args.ckpt_dir, exist_ok=True)
 if args.tmp is not None:
     os.makedirs(args.tmp, exist_ok=True)
 else:
     print("Error: tmp is not set")
-# Init log file
 logger = Logger(os.path.join(args.tmp, "log.txt"))
 logger_loss_file = os.path.join(args.tmp, "log_loss.txt")
 logger_loss_idx = 1
 
-# Init model
-device = torch.device("cuda")  # CUDA
+device = torch.device("cuda")  
 
 model = Network().to(device)
 
@@ -120,15 +106,11 @@ lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
     gamma=0.1
 )
 
-# Why freeze the backbone?...
 if config.freeze:
     for key, value in model.named_parameters():
         if 'bb.' in key:
             value.requires_grad = False
 
-# log model and optimizer params
-# logger.info("Model details:")
-# logger.info(model)
 logger.info("Optimizer details:")
 logger.info(optimizer)
 logger.info("Scheduler details:")
@@ -222,11 +204,6 @@ def main():
         if (epochs + 1) % 1 == 0 or epochs == 0:
             torch.save(model.state_dict(), args.tmp + '/model-' + str(epochs + 1) + '.pth')
 
-        # if epochs > 1:
-        # torch.save(model.state_dict(), args.tmp + '/model-' + str(epochs) + '.pth')
-    # dcfmnet_dict = model.dcfmnet.state_dict()
-    # torch.save(dcfmnet_dict, os.path.join(args.tmp, 'final.pth'))
-
 
 def train(epoch):
     loss_log = AverageMeter()
@@ -246,16 +223,7 @@ def train(epoch):
         scaled_preds = return_values[0]
         pred_contrast = return_values[5]
         norm_features = None
-        # if {'sal', 'contrast'} == set(config.loss):
-        # scaled_preds, pred_contrast = return_values[:2]
-        ##if config.GCAM_metric:
-        ##norm_features = return_values[-1]
-        ##scaled_preds = scaled_preds[-min(config.loss_sal_layers+int(bool(config.refine)), 4+int(bool(config.refine))):]
-
-        # Tricks
-        ##if config.GCAM_metric:
-        ##loss_sal, loss_triplet = sal_loss(scaled_preds, gts, norm_features=norm_features, labels=cls_gts)
-        ##else:
+    
         loss_scl = sclloss(return_values[1], return_values[2], return_values[3])
         loss_sal = sal_loss(scaled_preds, gts) + loss_scl * config.intra_contrast
 
@@ -270,11 +238,7 @@ def train(epoch):
             loss_ss = saliency_structure_consistency(sal_scale.sigmoid(), sal_s.sigmoid())
             loss_sal += loss_ss * 0.3
 
-        # Loss
-        # since there may be several losses for sal, the lambdas for them (lambdas_sal) are inside the loss.py
-        ##loss = loss_sal * 1.0
         loss = 0
-        # since there may be several losses for sal, the lambdas for them (lambdas_sal) are inside the loss.py
         loss_sal = loss_sal * 1
         loss += loss_sal
         if 'contrast' in config.loss:
@@ -301,12 +265,7 @@ def train(epoch):
             info_loss += ', Loss_total: {loss.val:.3f} ({loss.avg:.3f})  '.format(loss=loss_log)
             logger.info(''.join((info_progress, info_loss)))
     info_loss = '@==Final== Epoch[{0}/{1}]  Train Loss: {loss.avg:.3f}  '.format(epoch, args.epochs, loss=loss_log)
-    # if config.GCAM_metric:
-    # info_loss += 'Triplet Loss: {loss.avg:.3f}  '.format(loss=loss_log_triplet)
     logger.info(info_loss)
-    # if epoch % 1 == 0:
-    # torch.save(model.state_dict(), os.path.join(args.ckpt_dir, 'ep{}_checkpoint.pth'.format(epoch)))#
-
     return loss_log.avg
 
 
